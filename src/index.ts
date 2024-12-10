3627d18f5504335a1c47d111dd676ed7348b3468
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-10 19:40:06 +0100

    Added Task 4

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,29 @@
 // console.log(isStringEmpty("Hello"));
 // console.log(isStringEmpty(" "));
 
-///Task_3
+// ///Task_3
 
-type Compare<T> = (a: T, b: T) => boolean;
+// type Compare<T> = (a: T, b: T) => boolean;
 
-const areStringEqual: Compare<string> = (a, b) => a === b;
-const areNumbersEqual: Compare<number> = (a, b) => a === b;
+// const areStringEqual: Compare<string> = (a, b) => a === b;
+// const areNumbersEqual: Compare<number> = (a, b) => a === b;
 
-console.log(areStringEqual("Hello", "Hello"));
-console.log(areNumbersEqual(3, 3));
+// console.log(areStringEqual("Hello", "Hello"));
+// console.log(areNumbersEqual(3, 3));
+
+//Task_4
+
+function getLastElement<T>(array: T[]): T | undefined {
+  if (array.length === 0) {
+    return undefined;
+  }
+  return array[array.length - 1];
+}
+
+const numbers = [5, 8, 3, 15, 6, 7];
+const strings = ["laptop", "mouse", "keyboard", "monitor"];
+const emptyArray: number[] = [];
+
+console.log(getLastElement(numbers));
+console.log(getLastElement(strings));
+console.log(getLastElement(emptyArray));
